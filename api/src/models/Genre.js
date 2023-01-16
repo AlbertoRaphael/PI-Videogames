f3d098ef676ced3a09c78141f9c0c85d3239c7ee
Alberto <106848675+AlbertoRaphael@users.noreply.github.com>
2023-01-16 17:15:45 -0600

    modelo de la tabla de  generos

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -3,6 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo para GENRE 
+  
   sequelize.define('videogame', {
     id: {
       type: DataTypes.UUID,
