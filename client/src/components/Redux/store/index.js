b925e449da6f6b2bb08dc75d264241b2b9ac8499
Alberto <106848675+AlbertoRaphael@users.noreply.github.com>
2023-01-16 17:34:06 -0600

    Mddleware Thunk

--- client/src/components/Redux/store/index.js
+++ client/src/components/Redux/store/index.js
@@ -6,5 +6,4 @@ import rootReducer from "../reducer/index";
  const store = createStore(rootReducer, 
     composeWithDevTools(applyMiddleware(thunk))); // para que se puedan unar los devtools
  
- 
  export default store;
