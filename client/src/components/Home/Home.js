63e9ca51685e3dcf8727b3d67fb46f8574c74040
Alberto <106848675+AlbertoRaphael@users.noreply.github.com>
2023-01-16 17:27:12 -0600

    Pagina Principal donde se cargan las cartas y la barra de buscador y los ordenamientos

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -14,6 +14,7 @@ import Error from "./Error";
 import imagen from "../../img/portada.jpeg"
 import Loading from '../loading/Loading'
 
+
 export default function Home() {
 
     const [state, setState] = useState('') //me creo un estado local cuyo valor incial es vacio
