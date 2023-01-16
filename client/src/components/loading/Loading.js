f3d381fc83271b4ad288c125c739ba2bcaf47080
Alberto <106848675+AlbertoRaphael@users.noreply.github.com>
2023-01-16 17:29:26 -0600

    imagen de carga

--- client/src/components/loading/Loading.js
+++ client/src/components/loading/Loading.js
@@ -2,6 +2,7 @@ import React from 'react'
 import loading from '../../img/cargadorobot.gif'
 import s from './Loading.module.css'
 
+
 export default function PaginaDeCarga() {
     return (
           <div className={s.box_loading}>
